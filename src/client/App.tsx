4263be3d7bedbcba40c0ab3ec94ccab8546e3199
Harmandeep Singh <deepharman2000@gmail.com>
2021-04-08 18:36:24 -0400

    working template

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -5,10 +5,9 @@ import React from 'react';
 import Routers from './routers';
 
 // import './scss/app.scss'; //use this or the next line to use bootstrap
-import '../../node_modules/bootstrap/scss/bootstrap.scss';
+// import '../../node_modules/bootstrap/scss/bootstrap.scss';
 
 const dd = () => {
-    
     return(
         <React.Fragment>
             <Routers />
